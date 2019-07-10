265288ef758d30d5a2d3ef56e3dc00cab4c1772b
trilok <ram@hk.com>
2019-07-10 13:11:05 +0530

    secound update

--- lamars_site.py
+++ lamars_site.py
@@ -7,5 +7,35 @@ def hello():
     return render_template('index.html')
 
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/product")
+def product():
+    return render_template('product.html')
+
+
+@app.route("/screenshot")
+def screen_shot():
+    return render_template('screen_shot.html')
+
+
+# @app.route("/pricing")
+# def pricing():
+#     return render_template('pricing.html')
+
+
+@app.route("/client")
+def client():
+    return render_template('client.html')
+
+
+@app.route("/contact")
+def contact_us():
+    return render_template('contact.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
